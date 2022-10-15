11f67b83e930a3593ed92212047030f9ff42f8b3
Rémi Scandella <remi.scandella@gmail.com>
2022-10-15 22:02:56 +0200

    Add messageerror handler

--- modules/messageWorker.js
+++ modules/messageWorker.js
@@ -20,7 +20,7 @@ let workerReady = false;
 async function isWorkerReady() {
   if (workerReady) return true;
   
-  return new Promise(async resolve => {
+  return new Promise(async (resolve, reject) => {
     let tries = 1000;
     while (!workerReady && tries > 0) {
       const chan = new MessageChannel();
@@ -29,6 +29,7 @@ async function isWorkerReady() {
           resolve(workerReady = true);
         }
       };
+      chan.port1.onmessageerror(event => reject(event.data));
       worker.postMessage(
         JSON.stringify({ instruction: 'is-ready' }),
         [chan.port2]
@@ -36,18 +37,20 @@ async function isWorkerReady() {
       tries--;
       await new Promise(res => setTimeout(res, 50));
     }
+    reject(`Max tries exhausted: worker didn't answer in time`);
   });
 }
 
 
 export async function messageWorker(instruction, data) {
   await isWorkerReady();
-  const chan = new MessageChannel();
-  worker.postMessage(
-    JSON.stringify({ instruction, data }),
-    [chan.port2]
-  );
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
+    const chan = new MessageChannel();
     chan.port1.onmessage = event => resolve(JSON.parse(event.data));
+    chan.port1.onmessageerror = event => reject(event.data);
+    worker.postMessage(
+      JSON.stringify({ instruction, data }),
+      [chan.port2]
+    );
   });
 }
